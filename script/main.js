c8537f910d09b2fc5e5d167bd51fb3ca64460f06
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-10-09 04:21:37 +1100

    Add responsive design

--- script/main.js
+++ script/main.js
@@ -1,3 +1,17 @@
+// Nvagiation for small device
+
+var breadcrumb = document.getElementById("breadcrumb");
+var nav = document.getElementById("nav")
+
+breadcrumb.addEventListener("click",function(){
+  if (nav.className === "navigation") {
+    nav.className = "navigation show";
+  } else {
+    nav.className = "navigation";
+  }
+  });
+
+
 /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
 var acc = document.getElementsByClassName("accordion");
 var i;
