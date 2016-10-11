c7285e09e5c6d5eb388b66aff1f6ad9306fddc53
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-10-12 01:51:19 +1100

    Edit resume page

--- script/main.js
+++ script/main.js
@@ -19,6 +19,30 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
         this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
+        this.nextElementSibling.classList.toggle("showResume");
     }
 }
+
+// hide fixed scrolling contact details when it hits footer
+
+var contact_details = document.querySelector('.contact_details');
+var footer = document.querySelector('footer');
+
+function checkOffset() {
+  function getRectTop(el){
+    return  el.getBoundingClientRect().top;
+  }
+
+if (window.innerWidth >= 780) {
+  if((getRectTop(contact_details) + document.body.scrollTop) + contact_details.offsetHeight >= (getRectTop(footer) + document.body.scrollTop) - 10)
+    contact_details.style.position = 'absolute';
+  if(document.body.scrollTop + window.innerHeight < (getRectTop(footer) + document.body.scrollTop))
+    contact_details.style.position = 'fixed'; // restore when you scroll up
+
+  }
+}
+
+
+document.addEventListener("scroll", function(){
+  checkOffset();
+});
